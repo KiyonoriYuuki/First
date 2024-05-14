deda8fc1c91031c0578686a882940568457554bc
reustac1 <reustac1@binghamton.edu>
2024-05-14 16:23:46 -0400

    second commit

--- js/music-list.js
+++ js/music-list.js
@@ -28,6 +28,72 @@ let allMusic = [
       img: "knowing-you-exist",
       src: "knowing-you-exist"
     },
+    {
+      name: "Nobody Gets Me",
+      artist: "SZA",
+      img: "Nobody Gets Me",
+      src: "Nobody Gets Me"
+    },
+    {
+      name: "Back To December",
+      artist: "Taylor Swift",
+      img: "Back To December",
+      src: "Back To December"
+    },
+    {
+      name: "back to you",
+      artist: "Alexander Stewart",
+      img: "back to you",
+      src: "back to you"
+    },
+    {
+      name: "Don't Go",
+      artist: "Yatta Bandz",
+      img: "Don't Go",
+      src: "Don't Go"
+    },
+    {
+      name: "drunk text me",
+      artist: "Lexi Jayde",
+      img: "drunk text me",
+      src: "drunk text me"
+    },
+    {
+      name: "Fallin' Again (feat. Tatiana Manaois)",
+      artist: "Yatta Bandz & Tatiana Manaois",
+      img: "Fallin' Again",
+      src: "Fallin' Again (feat. Tatiana Manaois)"
+    },
+    {
+      name: "HEAVEN",
+      artist: "The Kid LAROI",
+      img: "HEAVEN",
+      src: "HEAVEN"
+    },
+    {
+      name: "Hold On (with Georgia Ku)",
+      artist: "ILLENIUM & Georgia Ku",
+      img: "Hold On",
+      src: "Hold On (with Georgia Ku)"
+    },
+    {
+      name: "i wish you cheated",
+      artist: "Alexander Stewart",
+      img: "i wish you cheated",
+      src: "i wish you cheated"
+    },
+    {
+      name: "if you only knew",
+      artist: "Alexander Stewart",
+      img: "if you only knew",
+      src: "if you only knew"
+    },
+    {
+      name: "Lookalike",
+      artist: "Conan Gray",
+      img: "Lookalike",
+      src: "Lookalike"
+    },
     
     // like this paste it and remember to give comma after ending of this bracket }
     // {
